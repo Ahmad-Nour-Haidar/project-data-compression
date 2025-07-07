4ed268c9e886eb4ddf524771e60b328eea5a4856
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2025-07-07 13:57:23 +0300

    refactor code

--- ProjectDataCompression/Functions/PasswordDialog.cs
+++ ProjectDataCompression/Functions/PasswordDialog.cs
@@ -1,12 +1,10 @@
 namespace ProjectDataCompression.Functions;
 
-using System.Windows.Forms;
-
 public static class PasswordDialog
 {
     public static string RequestPassword(string prompt = "Enter Password")
     {
-        Form promptForm = new Form()
+        Form promptForm = new Form
         {
             Width = 300,
             Height = 150,
@@ -17,12 +15,12 @@ public static class PasswordDialog
             MaximizeBox = false
         };
 
-        Label textLabel = new Label() { Left = 20, Top = 20, Text = prompt, AutoSize = true };
+        Label textLabel = new Label { Left = 20, Top = 20, Text = prompt, AutoSize = true };
 
-        TextBox passwordBox = new TextBox() { Left = 20, Top = 50, Width = 240, PlaceholderText = "Password" };
+        TextBox passwordBox = new TextBox { Left = 20, Top = 50, Width = 240, PlaceholderText = "Password" };
         passwordBox.UseSystemPasswordChar = true;
 
-        Button confirmation = new Button()
+        Button confirmation = new Button
             { Text = "OK", Left = 100, Width = 80, Top = 80, DialogResult = DialogResult.OK };
         confirmation.Click += (_, _) => { promptForm.Close(); };
 
