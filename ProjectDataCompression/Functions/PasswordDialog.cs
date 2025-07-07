d84f201e12cbe131d7046244ddd03d8a08457738
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2025-07-07 13:55:31 +0300

    move file PasswordDialog.cs

--- ProjectDataCompression/Functions/PasswordDialog.cs
+++ ProjectDataCompression/Functions/PasswordDialog.cs
@@ -1,4 +1,4 @@
-namespace ProjectDataCompression;
+namespace ProjectDataCompression.Functions;
 
 using System.Windows.Forms;
 
@@ -6,7 +6,6 @@ public static class PasswordDialog
 {
     public static string RequestPassword(string prompt = "Enter Password")
     {
-        // Create a new form
         Form promptForm = new Form()
         {
             Width = 300,
@@ -18,24 +17,20 @@ public static class PasswordDialog
             MaximizeBox = false
         };
 
-        // Label
         Label textLabel = new Label() { Left = 20, Top = 20, Text = prompt, AutoSize = true };
 
-        // Password TextBox
         TextBox passwordBox = new TextBox() { Left = 20, Top = 50, Width = 240, PlaceholderText = "Password" };
         passwordBox.UseSystemPasswordChar = true;
 
-        // OK button
         Button confirmation = new Button()
             { Text = "OK", Left = 100, Width = 80, Top = 80, DialogResult = DialogResult.OK };
-        confirmation.Click += (sender, e) => { promptForm.Close(); };
+        confirmation.Click += (_, _) => { promptForm.Close(); };
 
         promptForm.Controls.Add(textLabel);
         promptForm.Controls.Add(passwordBox);
         promptForm.Controls.Add(confirmation);
         promptForm.AcceptButton = confirmation;
 
-        // Show dialog and return password if OK clicked
         return promptForm.ShowDialog() == DialogResult.OK ? passwordBox.Text : "";
     }
 }
