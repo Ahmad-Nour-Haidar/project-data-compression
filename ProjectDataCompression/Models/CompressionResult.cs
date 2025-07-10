666d4b678d2320415f20ab0bcce83035318fded9
ahmad-nour-haidar <ahmadnourhaidar@gmail.com>
2025-07-10 08:40:26 +0300

    fix CompressionRatio CompressionResult.cs

--- ProjectDataCompression/Models/CompressionResult.cs
+++ ProjectDataCompression/Models/CompressionResult.cs
@@ -4,6 +4,6 @@ public class CompressionResult
 {
     public long OriginalSize { get; set; }
     public long CompressedSize { get; set; }
-    public double CompressionRatio => OriginalSize == 0 ? 0 : (double)CompressedSize / OriginalSize;
+    public double CompressionRatio => OriginalSize == 0 ? 0 : (1.0 - (double)CompressedSize / OriginalSize) * 100;
     public TimeSpan Duration { get; set; }
 }
